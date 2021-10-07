104b854da653dedf51cd7bc9f6aabdb498ad3c9d
trigiacbmt <88094935+trigiacbmt@users.noreply.github.com>
2021-10-07 18:03:30 +0700

    Add files via upload

---
+++ about-us/about-us.copmonent.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('aboutUsService')
+.component('aboutUs',{
+    templateUrl: "about-us/about-us.template.html",
+    controller: ['$http',function aboutUSController ($http){
+        $http.get(
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        )
+    }]
+})
